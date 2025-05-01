60cb1c8ada98257d84a0bc386a3f4c0c4db47ffe
Beatriz Sampaio <beatriz21sampaio@gmail.com>
2025-05-01 16:44:33 -0300

    java maven criado + banco de dados

---
+++ src/main/java/com/papelaria/catarse/CatarseApplication.java
@@ -0,0 +1,13 @@
+package com.papelaria.catarse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatarseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatarseApplication.class, args);
+    }
+
+}
